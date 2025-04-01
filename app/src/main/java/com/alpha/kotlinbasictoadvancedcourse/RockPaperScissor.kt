b6939bdb44a2601e1de6cca6586297a921d62b94
alphalabs72 <alphalabs729@gmail.com>
2025-04-01 14:02:09 +0800

    Added New Files

---
+++ app/src/main/java/com/alpha/kotlinbasictoadvancedcourse/RockPaperScissor.kt
@@ -0,0 +1,36 @@
+package com.alpha.kotlinbasictoadvancedcourse
+
+fun main(){
+    var computerChoice = ""
+    var playerChoice = ""
+
+    println("Rock, Paper, Scissor? Enter your choice")
+    playerChoice = readln()
+
+    val randomNumber = (1..3).random()
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        3 -> {
+            computerChoice = "Scissor"
+        }
+    }
+    println(computerChoice)
+
+    val winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissor" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissor" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+    if (winner == "Tie"){
+        println("It's a tie")
+    }else {
+        println("$winner won!")
+    }
+}
---
+++ app/src/main/java/com/alpha/kotlinbasictoadvancedcourse/WhileLoops.kt
@@ -0,0 +1,10 @@
+package com.alpha.kotlinbasictoadvancedcourse
+
+fun main(){
+    var count = 0
+    while (count < 5){
+        println("Count: $count")
+        count++
+    }
+    println("Loop is done!")
+}
