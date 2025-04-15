cf03b75bdc9182a4c70e0b4f1f38893101850fc8
alphalabs72 <alphalabs729@gmail.com>
2025-04-15 13:54:00 +0800

    added completed shopping list

--- app/src/main/java/com/alpha/kotlinbasictoadvancedcourse/ShoppingList.kt
+++ app/src/main/java/com/alpha/kotlinbasictoadvancedcourse/ShoppingList.kt
@@ -1,10 +1,15 @@
 package com.alpha.kotlinbasictoadvancedcourse
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -18,14 +23,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
 data class ShoppingItem(val id: Int,
                         var name: String,
                         var quantity: Int,
                         var isEditing: Boolean = false)
+@Composable
+fun getColorFromResources(colorId: Int): Color {
+    val context = LocalContext.current
+    return Color(context.getColor(colorId))
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Shopping(){
@@ -46,14 +65,51 @@ fun Shopping(){
         LazyColumn(
             modifier = Modifier.fillMaxSize().padding(16.dp)
         ) {
-            items(sItems) { item ->
-
+            items(sItems) { item->
+                if (item.isEditing){
+                    ShoppingItemEditor(item = item,onEditComplete ={
+                        editedName,editedQuantity ->
+                        sItems = sItems.map { it.copy(isEditing = false) }
+                        val editedItem = sItems.find { it.id == item.id }
+                        editedItem?.let {
+                            it.name = editedName
+                            it.quantity = editedQuantity
+                        }
+                    })
+                }else{
+                    ShoppingListItem(item = item,onEditClick = {
+                        sItems = sItems.map { it.copy(isEditing = it.id == item.id) }
+                    },
+                        onDeleteClick = {
+                            sItems = sItems-item
+                        })
+                }
             }
         }
     }
     if (showDialog){
        AlertDialog(onDismissRequest = {showDialog = false},
-           confirmButton = {},
+           confirmButton = {
+               Row(modifier = Modifier.fillMaxWidth().padding(8.dp),
+                   horizontalArrangement = Arrangement.SpaceBetween) {
+                   Button(onClick = {
+                       if (itemName.isNotBlank()){
+                           val newItem = ShoppingItem(
+                               id = sItems.size+1,
+                               name = itemName,
+                               quantity = itemQuantity.toInt()
+                           )
+                           sItems = sItems + newItem
+                           showDialog = false
+                           itemName = ""
+                           itemQuantity = ""
+                       }
+                   }) { Text("Add")}
+                   Button(onClick = {showDialog = false}) {
+                       Text("Cancel")
+                   }
+               }
+           },
            title = {Text("Add Shopping Item")},
            text = {
                Column {
@@ -70,6 +126,65 @@ fun Shopping(){
            )
     }
 }
+
+@Composable
+fun ShoppingItemEditor(item: ShoppingItem,onEditComplete:(String, Int) -> Unit){
+    var editedName by remember { mutableStateOf(item.name) }
+    var editedQuantity by remember { mutableStateOf(item.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+
+    Row (modifier = Modifier
+        .fillMaxWidth()
+        .background(Color.White)
+        .padding(8.dp)
+        , horizontalArrangement = Arrangement.SpaceEvenly){
+        Column {
+            BasicTextField(
+                value = editedName,
+                onValueChange = {editedName = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp))
+            BasicTextField(
+                value = editedQuantity,
+                onValueChange = {editedQuantity = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp))
+        }
+        Button(onClick = {
+            isEditing = false
+            onEditComplete(editedName,editedQuantity.toIntOrNull() ?: 1)
+        }) {
+            Text("Save")
+        }
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+){
+    Row(
+        modifier = Modifier.padding(8.dp).fillMaxWidth().border(
+            border = BorderStroke(2.dp,getColorFromResources(R.color.teal_700)),
+            shape = RoundedCornerShape(20)
+        ),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "Qty:  ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Row (modifier = Modifier.padding(8.dp)){
+            IconButton(onClick = onEditClick) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
+    }
+}
+
 @Preview(
     showBackground = true,
     showSystemUi = true
